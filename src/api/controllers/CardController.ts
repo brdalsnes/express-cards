d085c671febb133ef7a3051c9dbbb77592353907
Bjørnar Dalsnes <bjornar.dalsnes@knowit.no>
2020-07-23 21:26:06 +0200

    Error handling

--- src/api/controllers/CardController.ts
+++ src/api/controllers/CardController.ts
@@ -1,18 +1,21 @@
 import { Request, Response } from 'express';
+import asyncHandler from 'express-async-handler';
+import createError from 'http-errors';
 import CardService from '../services/CardService';
 
 class CardController {
-    static getAll = async (_req: Request, res: Response) => {
+    static getAll = asyncHandler(async (_req: Request, res: Response) => {
         const cardService = new CardService();
         const cards = await cardService.getAll()
         res.send(cards);
-    }
+    })
 
-    static get = async (req: Request, res: Response) => {
+    static get = asyncHandler(async (req: Request, res: Response) => {
         const cardService = new CardService();
         const card = await cardService.get(req.params.id);
+        if (!card) throw createError(400, 'Card not found');
         res.send(card);
-    }
+    })
 }
 
 export default CardController;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import 'reflect-metadata';
 import bodyParser from 'body-parser';
 import { createConnection } from 'typeorm';
+import createError from 'http-errors';
 require('dotenv').config();
 
 import routes from './api/routes';
@@ -9,13 +10,26 @@ import routes from './api/routes';
 const port = process.env.PORT || '8000';
 
 createConnection().then(() => {
-  const app = express();
-  app.use(bodyParser.json())
+    const app = express();
+    app.use(bodyParser.json())
 
-  app.use('/api', routes);
+    app.use('/api', routes);
 
-  app.listen(port, err => {
-    if (err) return console.error(err);
-    return console.log(`Server is listening on ${port}`);
-  });
+    app.use((_req: Request, _res: Response, next: NextFunction) => {
+        next(createError(404));
+    })
+
+    app.use((error: any, _req: Request, res: Response, _next: NextFunction) => {
+        res.status(error.status || 500);
+        res.json({
+            status: error.status,
+            message: error.message,
+            stack: error.stack
+        })
+    })
+
+    app.listen(port, err => {
+        if (err) return console.error(err);
+        return console.log(`Server is listening on ${port}`);
+    });
 })
